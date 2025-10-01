29774a282b5392ed092a275c732c264f1f03760a
Bruno Diogo Andre <triscal.bandre@terceiros.mag.com.br>
2025-09-30 21:01:17 -0300

    chore: some documentation

---
+++ src/Corporate.Cashflow.Application/UseCases/Balances/GetByDate/GetBalanceByDateQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corporate.Cashflow.Application.UseCases.Balances.GetByDate
+{
+    class GetBalanceByDateQuery
+    {
+    }
+}
